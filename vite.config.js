2f0eda8382eb994878a10ac1899a92e071fbff44
Ryan Trimble <mrtrimble2@gmail.com>
2024-03-14 13:44:07 -0400

    updates example to read a specified directory, get the list of folders, then loop through to create the zip files

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,22 @@
 import { defineConfig } from 'vite';
+import { readdirSync } from 'fs';
 import zipPack from 'vite-plugin-zip-pack';
+
+const getDirectories = (source) =>
+  readdirSync(source, { withFileTypes: true })
+    .filter((dirent) => dirent.isDirectory())
+    .map((dirent) => dirent.name);
+
+const zipPackModules = getDirectories('./src/downloads').map((module) => {
+  return zipPack({
+    inDir: `./src/downloads/${module}`,
+    outDir: `./dist/${module}`,
+    outFileName: `${module}`,
+  });
+});
 
 export default defineConfig({
   plugins: [
-    zipPack({
-      inDir: './src/downloads',
-      outDir: './dist/downloads',
-      outFileName: 'zipped-download.zip',
-    }),
+    zipPackModules
   ],
 });
